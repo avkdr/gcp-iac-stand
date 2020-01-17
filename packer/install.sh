198a1dd34ebcbf472e5e1fa58776a79ce693f237
Irina Radkova <radkova.irina.r@gmail.com>
2020-01-17 15:35:57 +0400

    add nexus to jenkins image

--- packer/install.sh
+++ packer/install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+echo 'Installing JDK'
 sudo yum install -y java-1.8.0-openjdk-devel
-sudo yum update
+sudo yum update -y
+
+echo 'Installing jenkins'
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install -y jenkins
+
+echo 'Installing git'
 yum install -y git
+
+echo 'Installing ansible'
 yum install -y ansible
+
+echo 'Installing wget'
 sudo yum install -y wget
+
 echo 'Downloading Maven'
 sudo yum install -y maven
 #wget https://www-us.apache.org/dist/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz -P /tmp
@@ -31,3 +41,28 @@ echo "${SSH_KEY}" | tee "/home/${USERNAME}/.ssh/authorized_keys"
 
 chmod 600 "/home/${USERNAME}/.ssh/authorized_keys"
 chown "${USERNAME}":"${USERNAME}" "/home/${USERNAME}/.ssh/authorized_keys"
+
+echo 'Create directory'
+sudo mkdir /app && cd /app
+
+echo 'Downloading nexus'
+
+sudo wget https://sonatype-download.global.ssl.fastly.net/repository/repositoryManager/3/nexus-3.19.1-01-unix.tar.gz
+tar xzf nexus-3.19.1-01-unix.tar.gz
+ln -s nexus-3.19.1-01 nexus
+
+echo 'Create Nexus user and provide permissions to nexus home directory'
+
+useradd nexus
+chown -R nexus:nexus /app/nexus
+
+echo 'Configure Nexus to run as Service in Linux machine'
+
+ln -s /app/nexus/bin/nexus /etc/init.d/nexus
+chown nexus:nexus /etc/init.d/nexus
+sudo chkconfig --add nexus
+sudo systemctl enable nexus
+
+echo 'Start the nexus service'
+
+sudo service nexus start
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,47 +1,41 @@
 provider "google"       {
   credentials              = "${file("../auth/key.json")}"
   project                  = "helical-ranger-260511"
-  zone                   = "europe-west1-b"
+  zone                     = "europe-west1-b"
 }
+
 resource "google_compute_instance" "apache" {
-  name                  = "gcp-centos7-apache"
-  machine_type          = "f1-micro"
+  name                     = "gcp-centos7-apache"
+  machine_type             = "f1-micro"
   network_interface     {
-    network             = "default"
+    network                = "default"
     access_config       {
     }
   }
   boot_disk {
   initialize_params {
-             image      = "gcp-centos7-apache"
+             image         = "gcp-centos7-apache"
     }
 }
 }
-resource "google_compute_instance" "jenkins" {
-  name                  = "jenkins-centos7-apache"
-  machine_type          = "f1-micro"
+
+resource "google_compute_address" "static" {
+  name                     = "static-ip-address"
+  address_type             = "EXTERNAL"
+}
+
+resource "google_compute_instance" "jenkins-nexus" {
+  name                     = "jenkins-nexus-centos7-apache"
+  machine_type             = "n1-standard-1"
   network_interface     {
-    network             = "default"
+    network                = "default"
     access_config       {
+      nat_ip               = "${google_compute_address.static.address}"
     }
   }
   boot_disk {
   initialize_params {
-             image      = "jenkins-centos7-apache"
+             image         = "jenkins-nexus-centos7-apache"
     }
-}
-}
-resource "google_compute_instance" "nexus" {
-  name                  = "nexus-centos7-apache"
-  machine_type          = "f1-micro"
-  network_interface     {
-    network             = "default"
-    access_config       {
-    }
-  }
-  boot_disk {
-  initialize_params {
-            image      = "nexus-centos7-apache"
-  }
 }
 }
